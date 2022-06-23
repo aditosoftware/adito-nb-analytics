994de4a06796e471d5dfd8b49c064936b6e6fa07
Werner Glanzer <w.glanzer@adito.de>
2022-06-23 10:41:14 +0200

    fix: InstallationID cached wrong adito version, if called too early

--- src/main/java/de/adito/aditoweb/nbm/metrics/impl/InstallationID.java
+++ src/main/java/de/adito/aditoweb/nbm/metrics/impl/InstallationID.java
@@ -1,6 +1,6 @@
 package de.adito.aditoweb.nbm.metrics.impl;
 
-import org.jetbrains.annotations.*;
+import org.jetbrains.annotations.NotNull;
 import org.openide.modules.OnStart;
 import org.openide.util.NbPreferences;
 
@@ -19,7 +19,7 @@ public class InstallationID
   private static final String _SYSTEMPROP_KEY = _ID_KEY;
   private static InstallationID _INSTANCE;
   private final String value;
-  private final String aditoVersion;
+  private String aditoVersion;
 
   /**
    * @return the instance of the current installation id
@@ -36,16 +36,15 @@ public class InstallationID
       if (installationIDText == null || installationIDText.trim().isEmpty())
         prefs.put(_ID_KEY, (installationIDText = UUID.randomUUID().toString()));
 
-      _INSTANCE = new InstallationID(installationIDText, System.getProperty(_VERSION_KEY));
+      _INSTANCE = new InstallationID(installationIDText);
     }
 
     return _INSTANCE;
   }
 
-  private InstallationID(@NotNull String pValue, @Nullable String pAditoVersion)
+  private InstallationID(@NotNull String pValue)
   {
     value = pValue;
-    aditoVersion = pAditoVersion == null || pAditoVersion.trim().isEmpty() ? "UNKNOWN" : pAditoVersion.replace('-', '_');
   }
 
   /**
@@ -54,7 +53,23 @@ public class InstallationID
   @NotNull
   public String asText()
   {
-    return value + "-" + aditoVersion;
+    return value + "-" + getVersion();
+  }
+
+  /**
+   * @return the adito version
+   */
+  @NotNull
+  public String getVersion()
+  {
+    if (aditoVersion == null)
+    {
+      aditoVersion = System.getProperty(_VERSION_KEY);
+      if (aditoVersion != null)
+        aditoVersion = aditoVersion.replace('-', '_');
+    }
+
+    return aditoVersion == null || aditoVersion.trim().isEmpty() ? "UNKNOWN" : aditoVersion;
   }
 
   /**
